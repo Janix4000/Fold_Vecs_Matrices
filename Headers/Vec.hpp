78653f50692e97d1c0521eeb469642631f42cc96
Janix4000 <janix4000@gmail.com>
2019-08-02 14:27:21 +0200

    Seperated Vec_Rows to .hpp

--- Headers/Vec.hpp
+++ Headers/Vec.hpp
@@ -5,61 +5,7 @@
 #include <tuple>
 #include <type_traits>
 
-template<size_t D, typename T>
-class Vec_Rows {
-	protected:
-		std::array<T, D> rows;
-	public:
-		T& operator[](size_t i) {
-			assert(i >= 0 && i < D);
-			return rows[i];
-		}
-		const T& operator[](size_t i) const {
-			assert(i >= 0 && i < D);
-			return rows[i];
-		}
-};
-
-
-template<typename T>
-class Vec_Rows<2, T> {
-public:
-	T x;
-	T y;
-public:
-	T& operator[](size_t row) {
-		assert(row == 0 || row == 1);
-		if (row == 0) return x;
-		return y;
-	}
-	const T& operator[](size_t row) const {
-		assert(row == 0 || row == 1);
-		if (row == 0) return x;
-		return y;
-	}
-};
-
-
-template<typename T>
-class Vec_Rows<3, T> {
-public:
-	T x;
-	T y;
-	T z;
-public:
-	T& operator[](size_t row) {
-		assert(row >= 0 && row < 3);
-		if (row == 0) return x;
-		if (row == 1) return y;
-		return z;
-	}
-	const T& operator[](size_t row) const {
-		assert(row >= 0 && row < 3);
-		if (row == 0) return x;
-		if (row == 1) return y;
-		return z;
-	}
-};
+#include "VecRows.hpp"
 
 
 template<size_t D, typename T, typename VecD>
@@ -127,9 +73,6 @@ public:
 		}
 		imp_assignOther(other, std::make_index_sequence<D>());
 	}
-
-
-
 
 	Vec& operator+=(const Vec& rhs) {
 		return imp_addeq(rhs, std::make_index_sequence<D>());
